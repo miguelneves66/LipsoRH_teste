8fb24c2b081ccc015509f887dca4667a8b20900a
miguel <migueltrabalhos75@gmail.com>
2026-05-13 00:42:02 -0300

    Add Holerite data class with employee details

--- app/src/main/java/com/example/lipsonrh/Holerite.kt
+++ app/src/main/java/com/example/lipsonrh/Holerite.kt
@@ -1,1 +1,10 @@
+package com.example.lipsonrh
 
+data class Holerite(
+    val nomeFuncionario: String,
+    val cargo: String,
+    val salario: Double,
+    val descontos: Double,
+    val mesReferencia: String,
+    val caminhoPdf: String
+)
